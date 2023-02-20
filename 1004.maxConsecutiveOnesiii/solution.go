bc3832d05590626dc0196ebce810034533e0bf79
leesper <pascal7718@gmail.com>
2023-02-20 17:18:19 +0800

    only one missed

--- 1004.maxConsecutiveOnesiii/solution.go
+++ 1004.maxConsecutiveOnesiii/solution.go
@@ -4,5 +4,29 @@ package leetcode1004
 // 未知：1个整数
 // 条件：要求的整数为nums数组中翻转k个0后连续1的最大个数
 func longestOnes(nums []int, k int) int {
-	return 0
+	left, right := 0, 0
+	max, timesOfZero := 0, 0
+
+	for right < len(nums) {
+		if nums[right] == 0 {
+			timesOfZero++
+		}
+
+		right++
+
+		if timesOfZero <= k && max < right-left {
+			max = right - left
+		}
+
+		for timesOfZero >= k {
+			if right >= len(nums) || nums[right] == 1 {
+				break
+			} else if nums[left] == 0 {
+				timesOfZero--
+			}
+			left++
+		}
+	}
+
+	return max
 }
--- 1004.maxConsecutiveOnesiii/solution_test.go
+++ 1004.maxConsecutiveOnesiii/solution_test.go
@@ -15,3 +15,24 @@ func TestCase2(t *testing.T) {
 		t.Errorf("got %d, want %d", result, 10)
 	}
 }
+
+func TestCase3(t *testing.T) {
+	result := longestOnes([]int{0, 0, 0, 0}, 0)
+	if result != 0 {
+		t.Errorf("got %d, want %d", result, 0)
+	}
+}
+
+func TestCase4(t *testing.T) {
+	result := longestOnes([]int{0, 0, 0, 1}, 4)
+	if result != 4 {
+		t.Errorf("got %d, want %d", result, 4)
+	}
+}
+
+func TestCase5(t *testing.T) {
+	result := longestOnes([]int{0, 1, 1}, 0)
+	if result != 2 {
+		t.Errorf("got %d, want %d", result, 2)
+	}
+}
